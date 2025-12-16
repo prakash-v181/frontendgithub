d0dcee890ea11cc724c5d104b5cc78cf69d69d11
prakash-v181 <prakashtare110@outlook.com>
2025-12-16 19:03:49 +0530

    Fix CORS for Vercel frontend

--- src/api.js
+++ src/api.js
@@ -8,16 +8,12 @@ const api = axios.create({
   withCredentials: true,
 });
 
-// Attach token automatically
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
